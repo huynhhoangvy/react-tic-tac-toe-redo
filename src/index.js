a2fdfa7aa2772fb0e62451884ba1f8ab7c6af8e9
huynhhoangvy <huynh.h.vy@gmail.com>
2019-06-05 18:11:03 +0700

    onClass work

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from 'react-google-login';
 
-// class Square extends React.Component {
-//     render() {
-//         return (
-//         <button 
-//         className="square" 
-//         onClick={() => this.props.onClick()}
-//         >
-//             {this.props.value}
-//         </button>
-//         );
-//   }
-// }
 
 const Square = (props) => {
     return (
@@ -32,6 +22,7 @@ class Board extends React.Component {
         this.state = ({
             squares: Array(9).fill(null),
             xIsNext: true,
+            highScores: [],
         })
     }
     renderSquare(i) {
@@ -48,24 +39,68 @@ class Board extends React.Component {
         }
         squares[i] = this.state.xIsNext ? "X" : "O";
         this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
         });
-    }
+      }
 
+      componentDidMount() {
+        this.fetchData()
+      }
+      
+     postData = async (name, elapsedTime) => {
+      let data = new URLSearchParams();
+      data.append('player', name);
+      data.append('score', 100 - elapsedTime);
+      const url = `http://ftw-highscores.herokuapp.com/tictactoe-dev`;
+      const response = await fetch(url,
+        {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: data.toString(),
+          json: true,
+        }
+      );
+    }
+  
+    async fetchData () {
+      const url = "http://ftw-highscores.herokuapp.com/tictactoe-dev";
+      let response = await fetch(url);
+      let data = await response.json();
+      this.setState({
+        highScores: data.items
+      })
+      console.log(this.state.highScores, "lelelelelelle")
+    }
+    
+    renderHighScore() {
+      console.log("rendering high scores", typeof(this.state.highScores))
+      return (
+      <h1>aaaaaaaaa</h1>
+      )
+      
+      // return this.state.highScores.map
+    }
+    
   render() {
     // const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
-    const winner = calculateWinner(this.state.squares);
-    let status;
+    const winner = calculateWinner(this.state.squares, this.props.name, this.props.timer, this.props.startTime);
+    let status, elapsedTime;
     if (winner) {
-        status = "Winner: " + winner;
+        status = winner;
+        console.log(this.props.starttime)
+        elapsedTime = Math.floor((Date.now() - this.props.startTime)/1000)
+        this.postData(this.props.name, elapsedTime)
     } else {
-        status = "Next player: " + (this.state.xIsNext ? "X" : "O")
+        status = " Next player: " + (this.state.xIsNext ? "X" : "O")
     }
 
     return (
       <div>
         <div className="status">{status}</div>
+        {elapsedTime !== undefined && <p>{"Elapsed Time: " + elapsedTime + " seconds"}</p>}
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -81,22 +116,69 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <ul>
+          {this.renderHighScore()}
+        </ul>
       </div>
     );
   }
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      isLoggedIn: false,
+      timer: 0,
+      startTime: 0,
+    }
+  }
+
+  componentDidMount() {
+    // setInterval(() => this.setState({ timer: Math.floor((Date.now() - this.state.startTime)/1000) }))
+    // setInterval(() => this.setState({ timer: this.state.timer + 1 }),1000)
+
+  }
+  responseGoogle = (response) => {
+    console.log(response);
+    // if (response.code === 200) {
+
+    // }
+    this.setState({
+      isLoggedIn: true,
+      name: response.w3.ofa,
+      startTime: Date.now(),
+
+    }, () => console.log(this.state))
+  }
+
   render() {
     return (
       <div className="game">
+        
+        
         <div className="game-board">
-          <Board />
+          { this.state.isLoggedIn && 
+          <div>
+            <Board name={this.state.name} timer={this.state.timer} startTime={this.state.startTime} /> 
+            <p>Player: {this.state.name}</p>
+          </div> }
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+        {this.state.isLoggedIn && 
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        }
+        {!this.state.isLoggedIn && 
+        <GoogleLogin
+          clientId="344739454450-js8adhd3b4cv0hvvfba9ee84v7ilatps.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        }
       </div>
     );
   }
@@ -109,7 +191,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
+function calculateWinner(squares, name, timer, startTime) {
+  console.log("log this squares", squares)
+  if (squares.every(square => square !== null)) {
+    return "DRAW"
+  }
     const winCases = [
         [0,1,2],
         [3,4,5],
@@ -123,8 +209,21 @@ function calculateWinner(squares) {
     for (let i = 0; i < winCases.length; i++) {
         const [a,b,c] = winCases[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+          console.log("is this triggered????", name,)
+          return "Winner is: " + squares[a];
         }
     }
     return null;
 }
+
+{/* <FacebookLogin
+  autoLoad={true}
+  appId="1088597931155576"
+  fields="name,email,picture"
+  callback={(resp) => this.responseFacebook(resp)}
+/>
+<GoogleLogin
+    clientId="344739454450-js8adhd3b4cv0hvvfba9ee84v7ilatps.apps.googleusercontent.com"
+    buttonText="Login"
+    cookiePolicy={'single_host_origin'}
+/> */}
